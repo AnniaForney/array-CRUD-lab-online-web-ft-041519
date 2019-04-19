6ce957b4a3fe47d321fcf2f6125cd6db0e70b3f7
Annia Forney <anniaforney@gmail.com>
2019-04-19 19:47:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  push.(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   unshift.(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pop.(element)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  shift.(element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return []
 end
 
 def retrieve_first_element_from_array(array)
