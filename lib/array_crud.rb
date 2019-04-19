a953f747dee93b6698417347bd20020a7b74e2f7
Annia Forney <anniaforney@gmail.com>
2019-04-19 19:59:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-array.first
+array.first(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array.last
+  array.last(array)
 end
